04e7dc1829728d980a06e36165ba3f614bef7bff
JohnSters <jonnyschoeman@mgail.com>
2023-03-16 12:47:34 +0200

    Added layout page.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<nav>
+  <ul>
+    <li><strong>CRUD App</strong></li>
+  </ul>
+  <ul class="navigation">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Articles</a></li>
+    <li><a href="#" role="button" class="secondary outline login">Login</a></li>
+  </ul>
+</nav>
+
+<!-- This is where the rest of the app will be rendered -->
+<slot></slot>
+
+<style>
+  .navigation {
+    --primary: #d81b60;
+  }
+
+  .login {
+    --secondary: #d81b60;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,36 @@
-<h1>Welcome</h1>
+<div class="grid">
+  <div>
+    <article>
+      <header>Your Story, Your Space 🚀</header>
+      Welcome to our little corner of the internet! Here, we gather some of the quirkiest and most interesting articles
+      from around the web to provide you with a one-stop-shop for your daily dose of information and entertainment.
+      <hr>
+      Whether you're into mind-bending science, heartwarming stories, or just looking for a good laugh, we've got you
+      covered. Our team of quirky writers scours the web to bring you the most interesting, thought-provoking, and
+      downright hilarious content out there.
+      <hr>
+      So sit back, relax, and get ready to embark on a journey of discovery and fun. We hope you enjoy your stay and
+      come back often for your daily fix of quirkiness!
+      <footer><a href="">+ Create your story now!</a></footer>
+    </article>
+  </div>
+</div>
+
+<style>
+  .grid {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 90vh;
+  }
+
+  article {
+    max-width: 800px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  a {
+    text-decoration: none;
+  }
+</style>
