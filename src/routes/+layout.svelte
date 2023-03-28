314725f87fe85f93fcca0d7f69810736fdb9fe99
JohnSters <jonnyschoeman@mgail.com>
2023-03-28 13:03:25 +0200

    Added functionality to add and edit articles, and also publish them using Prisma.io

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,24 @@
+<!--Navigation menu will be displayed on all pages-->
 <nav>
   <ul>
     <li><strong>CRUD App</strong></li>
   </ul>
   <ul class="navigation">
-    <li><a href="#">Home</a></li>
-    <li><a href="#">Articles</a></li>
+    <li><a href="/">Home</a></li>
+    <li><a href="/articles">Articles</a></li>
     <li><a href="#" role="button" class="secondary outline login">Login</a></li>
   </ul>
 </nav>
 
-<!-- This is where the rest of the app will be rendered -->
+<!--The slot tag opens up the other pages to be rendered-->
 <slot></slot>
 
 <style>
   .navigation {
-    --primary: #d81b60;
+    --primary: #00897b;
   }
 
   .login {
-    --secondary: #d81b60;
+    --secondary: #00897b;
   }
 </style>
---
+++ src/routes/articles/+page.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import type { PageData } from './$types'
+  export let data: PageData
+  $: ({ articles } = data)
+</script>
+
+<div class="grid">
+  <div>
+    <h2>Articles:</h2>
+    {#each articles as article}
+      <article>
+        <header><b>Title:</b> {article.title}</header>
+        <p>
+          {article.content}
+        </p>
+        <form action="?/deleteArticle&id={article.id}" method="POST">
+          <button type="submit" class="outline secondary delete">Delete Article</button>
+        </form>
+        <a href="/{article.id}" role="button" class="outline constrast" style="width: 100%;"
+        >Edit Article</a
+        >
+      </article>
+    {/each}
+  </div>
+  <form action="?/createArticle" method="POST">
+    <h3>New Article</h3>
+    <label for="title"> Title </label>
+    <input type="text" id="title" name="title" />
+    <label for="title"> Title </label>
+    <textarea id="content" name="content" rows={5} />
+    <button type="submit">Add Article</button>
+  </form>
+</div>
+
+<style>
+  .delete:hover {
+    color: darkred;
+    border-color: darkred;
+  }
+</style>
